1bf4c50e185db026560e7d41fd10171f538974b4
eyp <eyyup.sayilir@kolaysoft.com.tr>
2021-04-20 01:49:12 +0300

    Add project files.

---
+++ Business/Concrete/BasketManager.cs
@@ -0,0 +1,41 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BasketManager : IBasketService
+    {
+        IBasketDal _basketDal;
+        IProductService _productService;
+
+        //public BasketManager(IBasketDal basketDal, IProductService productService)
+        //{
+        //    _basketDal = basketDal;
+        //    _productService = productService;
+        //}
+        public BasketManager(IBasketDal basketDal)
+        {
+            _basketDal = basketDal;
+        //    _productService = productService;
+        }
+        public IResult Add(Basket basket)
+        {
+            //if (_productService.CheckStock(basket.ProductId)>0)
+            //{
+
+            //}
+            _basketDal.Add(basket);
+            return new SuccessResult("Ürün sepete eklendi");
+        }
+
+        public IDataResult<List<Basket>> GetAll()
+        {
+            return new SuccessDataResult<List<Basket>>(_basketDal.GetAll());
+        }
+    }
+}
---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            if(product.CategoryId == 1)
+            {
+
+                return new ErrorResult("Bu kaegoriye Urun kbul edilmiyor");
+            }
+            else {
+                _productDal.Add(product);
+            return new SuccessResult("Urun basairyla eklendi");
+        }
+        }
+
+        public IDataResult<List<Product>> GetAll()
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),"Ürünler Listelendi");
+        }
+
+        public IDataResult<List<Product>> GetAllByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>
+                 (_productDal.GetAll(p => p.CategoryId == categoryId),
+                 "Filtereli olarak listelide");
+        }
+    }
+}
